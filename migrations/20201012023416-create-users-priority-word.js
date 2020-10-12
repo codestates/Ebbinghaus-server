a95808c99167b41c3ddfddb998a61b8dbb7efff3
김현인 <rolenche@gmail.com>
2020-10-12 13:05:32 +0900

    settting dataBass
    associate로 스키마 구성, 데이타 베이스 에러 핸들링함

---
+++ migrations/20201012023416-create-users-priority-word.js
@@ -0,0 +1,30 @@
+'use strict';
+module.exports = {
+  up: async (queryInterface, Sequelize) => {
+    await queryInterface.createTable('users_priority_words', {
+      id: {
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+        type: Sequelize.INTEGER
+      },
+      user_id: {
+        type: Sequelize.INTEGER
+      },
+      priority_word_id: {
+        type: Sequelize.STRING
+      },
+      createdAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      },
+      updatedAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      }
+    });
+  },
+  down: async (queryInterface, Sequelize) => {
+    await queryInterface.dropTable('users_priority_words');
+  }
+};
--- models/timepasses.js
+++ models/timepasses.js
@@ -9,12 +9,15 @@ module.exports = (sequelize, DataTypes) => {
      * This method is not a part of Sequelize lifecycle.
      * The `models/index` file will call this method automatically.
      */
-    static associate(models) {
+    static associate(db) {
       // define association here
+      this.hasMany(db.minewords, { foreignKey: "user_id", sourceKey: "id" })
+      this.hasMany(db.users_priority_word, { foreignKey: "users_priority_word_id", sourceKey: "id" })
+
     }
   };
   TimePasses.init({
-    users_priority_world_id: DataTypes.INTEGER,
+    users_priority_word_id: DataTypes.INTEGER,
     MineWords_id: DataTypes.INTEGER,
     check_in: DataTypes.DATE,
     check_out: DataTypes.DATE,
