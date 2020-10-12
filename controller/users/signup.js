70791e4553e6c3b34210eab8cf5f3c48602fa6a8
김현인 <rolenche@gmail.com>
2020-10-12 17:44:22 +0900

    updateserver
    서버실행 관련 애러 핸들링함

--- controller/users/signup.js
+++ controller/users/signup.js
@@ -1,23 +1,47 @@
-const { users } = require('../../models/users');
+const users = require('../../models/users');
 
-module.exports = async (req, res) => {
-    const { name, password } = req.body;
+module.exports = {
+    post: async (req, res) => {
+        const { name, password } = req.body;
 
-    let [user, created] = await users.findOrCreate({
-        where: {
-            name
-        },
-        defaults: {
-            password
+        let [user, created] = await users.findOrCreate({
+            where: {
+                name
+            },
+            defaults: {
+                password
+            }
+        });
+
+        if (!created) {
+            return res.status(409).json({
+                message: "This email already exists",
+            })
         }
-    });
-
-    if (!created) {
-        return res.status(409).json({
-            message: "This email already exists",
+        return res.status(201).json({
+            message: "Successfully registerd"
         })
     }
-    return res.status(201).json({
-        message: "Successfully registerd"
-    })
 }
+
+// module.exports = async (req, res) => {
+//     const { name, password } = req.body;
+
+//     let [user, created] = await users.findOrCreate({
+//         where: {
+//             name
+//         },
+//         defaults: {
+//             password
+//         }
+//     });
+
+//     if (!created) {
+//         return res.status(409).json({
+//             message: "This email already exists",
+//         })
+//     }
+//     return res.status(201).json({
+//         message: "Successfully registerd"
+//     })
+// }
