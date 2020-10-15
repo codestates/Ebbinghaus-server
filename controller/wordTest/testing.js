c8cd981b7182a36b003fcffd1ace83c8dd3ab672
김현인 <rolenche@gmail.com>
2020-10-15 16:57:04 +0900

    a

--- controller/wordTest/testing.js
+++ controller/wordTest/testing.js
@@ -1,1 +1,12 @@
-module.exports = {};
+const { user, mineWord } = require("../../models");
+
+module.exports = {
+  get: (req, res) => {
+    let { userid } = req.session;
+    if (userid) {
+      res.status(200).json("라우터 연결 완료");
+    } else {
+      res.status(400).send("잘못된 요청");
+    }
+  },
+};
--- routes/words.js
+++ routes/words.js
@@ -2,11 +2,13 @@ const express = require("express");
 const router = express.Router();
 const { wordsController } = require("../controller");
 
+// 나만의 영단어 리스트
+router.get("/mine", wordsController.mineWordList.get);
+
 //나만의 영단어 등록
 router.post("/mine/register", wordsController.mineWordRegister.post);
 
-router.get("/mine", wordsController.mineWordList.get);
-
+//나만의 영단어 테스트 등록
 router.post("/mine/test-register", wordsController.mineTestRegister.post);
 
 module.exports = router;
