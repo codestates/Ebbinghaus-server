fd1f80ed1d65f8590ade7c6304ccd8decbe17f27
김현인 <rolenche@gmail.com>
2020-10-19 18:07:24 +0900

    update foreach
    foreach해당되는 컴포넌트 수정

---
+++ controller/words/mineWordTestButton.js
@@ -0,0 +1,37 @@
+const { user, mineWord } = require("../../models");
+const sequelize = require("sequelize");
+
+module.exports = {
+  get: (req, res) => {
+    let { userid } = req.session;
+    if (userid) {
+      user
+        .findOne({
+          where: {
+            id: userid.id,
+          },
+        })
+        .then((data) => {
+          mineWord
+            .findAll({
+              raw: true,
+              where: {
+                user_id: data.id,
+                distinguish: {
+                  [sequelize.Op.or]: [0, 1, 3, 7, 15, 100],
+                },
+              },
+              order: [["id", "DESC"]],
+            })
+            .then((data) => {
+              if (data) {
+                res.status(200).json(data);
+              } else {
+                res.status(404).send("잘못됬어");
+              }
+            })
+            .catch((err) => console.error("error", err));
+        });
+    }
+  },
+};
---
+++ controller/words/priorityWordTestButton.js
@@ -0,0 +1,72 @@
+const { user, priorityWord, user_priority_word } = require("../../models");
+const sequelize = require("sequelize");
+
+module.exports = {
+  get: (req, res) => {
+    let { userid } = req.session;
+    if (userid) {
+      console.log("user:", userid);
+      user
+        .findOne({
+          where: {
+            id: userid.id,
+          },
+        })
+        .then((data) => {
+          if (data) {
+            user_priority_word
+              .findAll({
+                raw: true,
+                where: {
+                  user_id: data.id,
+                  distinguish: {
+                    [sequelize.Op.or]: [0, 1, 3, 7, 15, 100],
+                  },
+                },
+                order: [["id", "DESC"]],
+              })
+              .then(async (data) => {
+                if (data) {
+                  for (let i = 0; i < data.length; i++) {
+                    // console.log("데이타", data.priority_word_id);
+                    priorityWord
+                      .findAll({
+                        raw: true,
+                        where: {
+                          id: data[i].priority_word_id,
+                        },
+                      })
+                      .then((data) => {
+                        console.log("데이타", data);
+                        if (data) {
+                          res.status(200).json(data);
+                        } else {
+                          res.status(404).send("잘못됬어");
+                        }
+                      })
+                      .catch((err) => console.error("error", err));
+                  }
+                  let data1 = await data.forEach((word) => {
+                    priorityWord.findAll({
+                      raw: true,
+                      where: {
+                        id: word.priority_word_id,
+                      },
+                    });
+                  });
+                  console.log("데이타", data1);
+                }
+              })
+              .then((data) => {
+                if (data) {
+                  res.status(200).json(data);
+                } else {
+                  res.status(404).send("잘못됬어");
+                }
+              })
+              .catch((err) => console.error("error", err));
+          }
+        });
+    }
+  },
+};
