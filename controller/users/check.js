bd4f4ad9c2dd1ad6c453a70e1675174f06acb9d1
아이슥 <leesak@aiseug-ui-MacBookPro.local>
2020-10-21 20:58:19 +0900

    update check
    아이디 체크 기능

--- controller/users/check.js
+++ controller/users/check.js
@@ -2,13 +2,15 @@ const { user } = require("../../models");
 
 module.exports = {
   get: async (req, res) => {
+    const { id } = req.users;
     try {
       let users = await user.findOne({
-        where: { id: users.id },
-        attributes: ["id", "name"],
+        where: { id: id },
+        attributes: ["id", "name", "access_token"],
       });
+      console.log("users:", users);
       if (!users) throw Error("유저가 존재하지 않습니다.");
-      res.json(users);
+      res.send(users);
     } catch (e) {
       console.log("error:", e);
       res.status(400), json({ message: e.message });
--- controller/users/mypage.js
+++ controller/users/mypage.js
@@ -1,36 +1,27 @@
 const { user, mineWord } = require("../../models");
 
 module.exports = {
-  get: (req, res) => {
+  get: async (req, res) => {
     const { id } = req.users;
-    // const users =  user.findByPk(id);
 
-    if (id) {
-      user
-        .findByPk({
+    user.findByPk(id).then((data) => {
+      mineWord
+        .findAll({
+          raw: true,
           where: {
-            id: id,
+            user_id: data.id,
+            distinguish: 99,
           },
+          order: [["id", "DESC"]],
         })
-        .then((data) => {
-          mineWord
-            .findAll({
-              raw: true,
-              where: {
-                user_id: data.id,
-                distinguish: 99,
-              },
-              order: [["id", "DESC"]],
-            })
-            .then((data1) => {
-              if (data1) {
-                res.status(200).json(data);
-              } else {
-                res.status(404).send("잘못됬어");
-              }
-            })
-            .catch((err) => console.error("error", err));
-        });
-    }
+        .then((data1) => {
+          if (data1) {
+            res.status(200).json(data);
+          } else {
+            res.status(404).send("잘못됬어");
+          }
+        })
+        .catch((err) => console.error("error", err));
+    });
   },
 };
